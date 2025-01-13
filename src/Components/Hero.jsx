095f84699d4e05e6869bec883c0c6474f776c5e9
kaleem ullah <gulkhan97115@gmail.com>
2025-01-13 16:44:14 +0500

    added

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
           destinations. Your adventure begins here—book your Swat Valley tour
           today!
         </p>
-        <ul className="w-full h-auto lg:items-start  rounded shadow-xl lg:w-[50%] lg:flex  lg:justify-center lg:p-4 lg:gap-8 transition-all duration-200 hover:scale-105 hover:shadow-slate-800 lg:flex-col">
+        <ul className="w-full h-auto lg:items-start  rounded shadow-xl lg:w-[50%] lg:flex  lg:justify-center lg:p-4 lg:gap-8 transition-all mt-4 duration-200 hover:scale-105 hover:shadow-slate-800 lg:flex-col">
           <li className="flex items-center w-full gap-2 p-2 text-lg border-b-2 sm:text-lg md:text-lg lg:text-2xl xl:text-3xl border-slate-300">
             <img
               src={destination}
@@ -50,23 +50,23 @@ const Hero = () => {
         <p className="text-lg text-slate-900 font-Manrope">
           Start Your Journey
         </p>
-        <button class="relative flex items-center  xs:px-6 xs:py-3 overflow-hidden font-medium transition-all bg-indigo-500 rounded-md group">
-          <span class="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-indigo-700 rounded group-hover:-mr-4 group-hover:-mt-4">
-            <span class="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
+        <button className="relative flex items-center overflow-hidden font-medium transition-all bg-indigo-500 rounded-md xs:px-6 xs:py-3 group">
+          <span className="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-indigo-700 rounded group-hover:-mr-4 group-hover:-mt-4">
+            <span className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
           </span>
-          <span class="absolute bottom-0 rotate-180 left-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-indigo-700 rounded group-hover:-ml-4 group-hover:-mb-4">
-            <span class="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
+          <span className="absolute bottom-0 left-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out rotate-180 bg-indigo-700 rounded group-hover:-ml-4 group-hover:-mb-4">
+            <span className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
           </span>
-          <span class="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full bg-indigo-600 rounded-md group-hover:translate-x-0"></span>
-          <span class="relative w-full  text-white transition-colors duration-200 ease-in-out text-center group-hover:text-white">
+          <span className="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full bg-indigo-600 rounded-md group-hover:translate-x-0"></span>
+          <span className="relative w-full text-center text-white transition-colors duration-200 ease-in-out group-hover:text-white">
             <Link to={"/packages"}>Explore Packages</Link>
           </span>
         </button>
-        <div class="overflow-x-visible relative w-14 h-14 overflow-y-clip group text-center">
-          <div class="flex justify-center items-center w-14 h-14 rounded-full bg-slate-900 transition-all duration-300 absolute top-0 group-hover:scale-[.60] group-hover:origin-top text-white">
+        <div className="relative overflow-x-visible text-center w-14 h-14 overflow-y-clip group">
+          <div className="flex justify-center items-center w-14 h-14 rounded-full bg-slate-900 transition-all duration-300 absolute top-0 group-hover:scale-[.60] group-hover:origin-top text-white">
             <img src={car} alt="Car" className="w-10 h-10" />
           </div>
-          <div class="absolute  font-bold -bottom-10 left-1/2 text-sm text-center text-black whitespace-nowrap transition-all duration-300 transform -translate-x-1/2 group-hover:bottom-0">
+          <div className="absolute text-sm font-bold text-center text-black transition-all duration-300 transform -translate-x-1/2 -bottom-10 left-1/2 whitespace-nowrap group-hover:bottom-0">
             Book Now
           </div>
         </div>
