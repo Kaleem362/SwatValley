2589a99bae795945c8dd3d6b9a21408543f02559
kaleem ullah <gulkhan97115@gmail.com>
2025-01-14 01:10:55 +0500

    worked alot in services added the cards and then content of why visit swat valley

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import vehicle from "../assets/Valley images/carcamping.jpg";
+import CozyHotels from "../assets/Valley images/PC hotel malamjabba.jpg";
+import Travel from "../assets/Valley images/travel.jpg";
+import { Link } from "react-router";
 
 const Services = () => {
   return (
@@ -6,44 +10,184 @@ const Services = () => {
       <h1 className="w-full mb-2 text-4xl font-bold text-slate-800 text-start xs:text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-Manrope">
         Services We offer
       </h1>
-      <div className="card-container">
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div className="flex flex-wrap justify-center w-full gap-6 p-4 card-container">
+        {/* card */}
+        <div className="w-64 h-auto bg-white border-2 rounded-lg shadow xs border-slate-300">
           <a href="#">
-            <img class="rounded-t-lg" src={""} alt="" />
+            <img
+              className="object-cover w-full h-auto rounded-t-lg"
+              src={vehicle}
+              alt=""
+            />
           </a>
-          <div class="p-5">
+          <div className="px-4 py-2 ">
             <a href="#">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              <h5 className="text-lg font-bold tracking-tight text-gray-900 sm:text-xl md:text-2xl">
                 4x4 Off-Road Adventure
               </h5>
             </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
+            <p className="mb-4 text-sm font-normal text-gray-700 sm:text-base md:text-lg">
+              We provide 4x4 vehicles for a comfortable ride both off-road and
+              on-road. Your safety is our top priority.
             </p>
-            <a
-              href="#"
-              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            <Link
+              to={""}
+              className="inline-flex items-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-slate-800 hover:bg-slate-900"
             >
-              Read more
+              Explore
               <svg
-                class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                aria-hidden="true"
+                className="w-4 h-4 ml-2 rtl:rotate-180"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 14 10"
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M1 5h12m0 0L9 1m4 4L9 9"
                 />
               </svg>
-            </a>
+            </Link>
           </div>
         </div>
+        {/* card */}
+        <div className="w-64 h-auto bg-white border-2 rounded-lg shadow xs border-slate-300">
+          <a href="#">
+            <img
+              className="object-cover w-full h-auto rounded-t-lg"
+              src={CozyHotels}
+              alt=""
+            />
+          </a>
+          <div className="px-4 py-2 ">
+            <a href="#">
+              <h5 className="text-lg font-bold tracking-tight text-gray-900 sm:text-xl md:text-2xl">
+                Cozy Hotels
+              </h5>
+            </a>
+            <p className="mb-4 text-sm font-normal text-gray-700 sm:text-base md:text-lg">
+              Enjoy comfort and luxury in our cozy hotels with exceptional
+              service and serene locations, our hotel provides top-notch
+              amnities.
+            </p>
+            <Link
+              to={""}
+              className="inline-flex items-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-slate-800 hover:bg-slate-900"
+            >
+              Explore
+              <svg
+                className="w-4 h-4 ml-2 rtl:rotate-180"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </Link>
+          </div>
+        </div>
+        {/* card */}
+        <div className="w-64 h-auto bg-white border-2 rounded-lg shadow xs border-slate-300">
+          <a href="#">
+            <img
+              className="object-cover w-full h-auto rounded-t-lg"
+              src={Travel}
+              alt=""
+            />
+          </a>
+          <div className="px-4 py-2 ">
+            <a href="#">
+              <h5 className="text-lg font-bold tracking-tight text-gray-900 sm:text-xl md:text-2xl">
+                Travel with Ease
+              </h5>
+            </a>
+            <p className="mb-4 text-sm font-normal text-gray-700 sm:text-base md:text-lg">
+              Travel with ease and discover new horizons with seamless planning
+              and curated experiences. Enjoy a hassle-free journey creating
+              memories.
+            </p>
+            <Link
+              to={""}
+              className="inline-flex items-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-slate-800 hover:bg-slate-900"
+            >
+              Explore
+              <svg
+                className="w-4 h-4 ml-2 rtl:rotate-180"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </Link>
+          </div>
+        </div>
+        <div className="w-64 h-auto bg-white border-2 rounded-lg shadow xs border-slate-300">
+          <a href="#">
+            <img
+              className="object-cover w-full h-auto rounded-t-lg"
+              src={vehicle}
+              alt=""
+            />
+          </a>
+          <div className="px-4 py-2 ">
+            <a href="#">
+              <h5 className="text-lg font-bold tracking-tight text-gray-900 sm:text-xl md:text-2xl">
+                4x4 Off-Road Adventure
+              </h5>
+            </a>
+            <p className="mb-4 text-sm font-normal text-gray-700 sm:text-base md:text-lg">
+              We provide 4x4 vehicles for a comfortable ride both off-road and
+              on-road. Your safety is our top priority.
+            </p>
+            <Link
+              to={""}
+              className="inline-flex items-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-slate-800 hover:bg-slate-900"
+            >
+              Explore
+              <svg
+                className="w-4 h-4 ml-2 rtl:rotate-180"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="whyChooseSwat">
+        <h1 className="w-full mb-2 text-4xl font-bold text-slate-800 text-start xs:text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-Manrope">
+          Why Visit Swat Valley?
+        </h1>
+        <p className="p-4 my-4 xs:text-md sm:text-lg lg:text-xl  bg-white rounded-lg shadow-lg  xs:w-[400px] sm:w-[520px] md:w-[600px] lg:w-[700px] text-slate-500">
+          <b>Welcome to Swat Valley,</b> the Switzerland of the East! Whether
+          you're seeking adventure, relaxation, or a mix of both, our tour
+          services are designed to provide an unforgettable experience. From
+          serene valleys and majestic peaks to luxurious accommodations and
+          hassle-free travel, we ensure your trip is seamless and extraordinary.
+        </p>
       </div>
     </div>
   );
