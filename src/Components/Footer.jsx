89744b9ac7e2c0e757aab9edc765cd6a970d2e76
kaleem ullah <gulkhan97115@gmail.com>
2025-01-14 11:46:57 +0500

    spacing

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,13 +3,15 @@ import logo from "../assets/logo/logo.png";
 
 const Footer = () => {
   return (
-    <div className="flex items-center justify-around w-full h-auto bg-green-400">
-      <div className="logo">
-        <img
-          src={logo}
-          alt="Logo"
-          className="h-auto w-60 xs:h-16 xs:w-16 sm:w-24 sm-h-24 md:h-32 md:w-32 lg:h-60 lg:w-60"
-        />
+    <div className="flex items-start justify-around w-full h-auto bg-green-400">
+      <img
+        src={logo}
+        alt="Logo"
+        className="w-16 h-16 sm:w-24 sm-h-24 md:h-32 md:w-32 lg:h-60 lg:w-60"
+      />
+
+      <div className="tit">
+        <h1>The Swat Tours</h1>
       </div>
     </div>
   );
