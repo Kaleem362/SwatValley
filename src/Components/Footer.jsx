3fe46f9554779c8b578757727a035dba35f134b0
kaleem ullah <gulkhan97115@gmail.com>
2025-01-14 12:55:50 +0500

    responsive footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,24 +4,24 @@ import { Link } from "react-router";
 
 const Footer = () => {
   return (
-    <div className="items-center justify-around w-full p-8 text-white bg-green-900 xs:flex xs:flex-col sm:flex sm:flex-row md:flex md:flex-row lg:flex lg:flex-row">
+    <div className="items-start justify-around w-full p-8 text-white bg-green-900 xs:flex xs:flex-col sm:flex sm:flex-row md:flex md:flex-row lg:flex lg:flex-row">
       <img
         src={logo}
         alt="Logo"
         className="xs:w-48 xs:h-48 sm:w-24 sm-h-24 md:h-32 md:w-32 lg:h-48 lg:w-48"
       />
 
-      <div className="flex-col items-start justify-center h-full title min-w-80">
-        <h1 className="w-full text-5xl font-extrabold text-slate-400">
+      <div className="flex-col items-start justify-center h-full title md:w-96">
+        <h1 className="w-full my-2 text-5xl font-extrabold text-center text-slate-100">
           The Swat Tours
         </h1>
-        <p className="text-slate-400 w-80">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-          minus sunt delectus tempore dignissimos consequuntur possimus vitae
-          aliquam quidem voluptas?
+        <p className="my-2 text-slate-100 md:w-fit">
+          Choose <b>The Swat Tours</b> for a stress-free and unforgettable tour,
+          where every detail is taken care of with care and expertise. Let’s
+          make your travel dreams a reality!
         </p>
       </div>
-      <div className="flex flex-col items-start justify-center w-full h-full gap-2 pages-content ">
+      <div className="flex flex-col items-start justify-center w-full h-full gap-2 pages-content md:w-fit ">
         <h4 className="w-full text-lg text-slate-200 hover:underline font-manrope">
           {" "}
           <Link rel="stylesheet" href="">
@@ -46,14 +46,14 @@ const Footer = () => {
             Contact us
           </Link>
         </h4>
-        <h4 className="hover:underline text-slate-200">
+        <h4 className="text-lg hover:underline font-manrope text-slate-200">
           {" "}
           <Link rel="stylesheet" href="">
             About us
           </Link>
         </h4>
       </div>
-      <div className="flex flex-col w-full gap-2 pt-4 mt-4 border-t-2 border-slate-900 misc-pages text-slate-200">
+      <div className="flex flex-col items-start w-full gap-2 mt-4 xs:pt-4 md:p-0 md:w-fit misc-pages text-slate-200">
         <h4 className="text-lg hover:underline font-manrope">
           <Link>Privacy Policy</Link>
         </h4>
