22d1eb4d69ad8b1e47666028861a71675965260f
kaleem ullah <gulkhan97115@gmail.com>
2025-02-11 11:45:11 +0500

    fasdf

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -195,7 +195,7 @@ const Contact = () => {
         <link rel="canonical" href="https://theswattours.com/contact" />
       </Helmet>{" "}
       <div className="container w-full p-6 mx-auto my-4">
-        <h1 className="w-full my-2 text-4xl font-bold text-center text-slate-800 xs:text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-Manrope animate-fadeInFromLeft">
+        <h1 className="w-full my-2 text-4xl font-bold text-center text-slate-800 xs:text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-Manrope animate-fadeInFromTop">
           Contact Us
         </h1>
 
